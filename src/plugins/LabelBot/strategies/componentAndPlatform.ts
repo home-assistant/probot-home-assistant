ee750f2424b65d5429eec7d822d151748119d24d
Paulus Schoutsen <balloob@gmail.com>
2020-04-03 14:42:06 -0700

    Improve integration labeling of PRs (#34)

--- src/plugins/LabelBot/strategies/componentAndPlatform.ts
+++ src/plugins/LabelBot/strategies/componentAndPlatform.ts
@@ -1,9 +1,7 @@
 import { PRContext } from "../../../types";
 import { ParsedPath } from "../../../util/parse_path";
 
-const VALID_TYPES = ["component", "platform"];
-
 export default (context: PRContext, parsed: ParsedPath[]) =>
   parsed
-    .filter((file) => VALID_TYPES.includes(file.type))
+    .filter((file) => file.component)
     .map((file) => `integration: ${file.component}`);
