9d9ab1e59012a36129c180a5b22fae49a3e868ad
Joakim Sørensen <joasoe@gmail.com>
2020-11-26 11:33:56 +0100

    Add support for platforms (#66)
    Co-authored-by: Franck Nijhof <git@frenck.dev>

--- test/plugins/SetIntegration/set_integration.spec.ts
+++ test/plugins/SetIntegration/set_integration.spec.ts
@@ -65,4 +65,18 @@ describe(NAME, () => {
       labels: ["integration: awesome_integration"],
     });
   });
+
+  it("Integration with platform", async () => {
+    mockContext.payload.issue.body =
+      "Link: https://www.home-assistant.io/integrations/platform.awesome";
+    getLabelResponse = {
+      status: 200,
+      data: { name: "integration: awesome" },
+    };
+    await runSetIntegration(mockContext);
+
+    assert.deepStrictEqual(setLabels, {
+      labels: ["integration: awesome"],
+    });
+  });
 });
