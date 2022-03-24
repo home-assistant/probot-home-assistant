4eded81865c5610018a0ec19ce17e7d5208a078f
Paulus Schoutsen <balloob@gmail.com>
2022-03-24 11:35:06 -0700

    Add config flow label (#104)
    * Add config flow label
    
    * Only add config flow label if not a new integration
    
    * Compare against test init file
    
    * YA

---
+++ src/plugins/LabelBot/strategies/configFlow.ts
@@ -0,0 +1,26 @@
+import { PRContext } from "../../../types";
+import { ParsedPath } from "../../../util/parse_path";
+
+export default function (context: PRContext, parsed: ParsedPath[]) {
+  const addedFlows = new Set(
+    parsed
+      .filter(
+        (fil) =>
+          fil.type == "component" &&
+          fil.status == "added" &&
+          fil.filename === "config_flow.py"
+      )
+      .map((fil) => fil.component)
+  );
+  // remove new integrations
+  for (const fil of parsed) {
+    if (
+      fil.type == "component" &&
+      fil.status == "added" &&
+      fil.filename === "__init__.py"
+    ) {
+      addedFlows.delete(fil.component);
+    }
+  }
+  return addedFlows.size ? ["config-flow"] : [];
+}
---
+++ test/plugins/LabelBot/strategies/configFlow.spec.ts
@@ -0,0 +1,44 @@
+import * as assert from "assert";
+
+import configFlow from "../../../../src/plugins/LabelBot/strategies/configFlow";
+
+import { ParsedPath } from "../../../../src/util/parse_path";
+
+function getOutput(files, status) {
+  var output = configFlow(
+    null,
+    files.map(
+      (filename) =>
+        new ParsedPath(
+          // @ts-ignore
+          {
+            filename,
+            status,
+          }
+        )
+    )
+  );
+  return output.length ? output[0] : null;
+}
+
+describe("configFlow", () => {
+  it("add new integration", () => {
+    assert.deepEqual(
+      getOutput(
+        [
+          "homeassistant/components/http/__init__.py",
+          "homeassistant/components/http/config_flow.py",
+        ],
+        "added"
+      ),
+      null
+    );
+  });
+
+  it("add config flow to existing integration", () => {
+    assert.deepEqual(
+      getOutput(["homeassistant/components/mqtt/config_flow.py"], "added"),
+      "config-flow"
+    );
+  });
+});
